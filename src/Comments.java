56be6bc8470ffa5f1829a6165fa88c533b149e3a
omkar-s8203 <omkar876742@gmail.com>
2025-07-15 16:45:13 +0530

    Add Comments class with main method and example comments

---
+++ src/Comments.java
@@ -0,0 +1,30 @@
+public class Comments {
+    public static void main(String[] args) {
+
+        /**
+         *
+         * hiii this my main method
+         * @param args
+         */
+        System.out.println("comments demonstration");
+
+// this is important don't delete this
+        /*
+        * Hii DEveloper is omkar suryawanshi who prepare this shit
+        * plaese contribute if you open this file.
+        * here im here for learing purpose
+        * */
+
+        /*
+        hi
+        yhis id omkar sureness hq js
+        smsfk
+        ifsj
+        sfjfk
+        fn
+        fnkjd
+        nfisfh f kkjkh
+        */
+
+    }
+}
