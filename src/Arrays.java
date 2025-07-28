2a7455189fdc44d981f6eadbba9b0be931db3e39
omkar-s8203 <omkar876742@gmail.com>
2025-07-29 00:10:14 +0530

    chapter 3 java

---
+++ src/Arrays.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int arr1[]= new int[5];
+        int numbers[]={1,2,3,4};
+        int moreNumbers[]={1,2,3,4,5};
+String fruite[] = {"apple", "banana", "watermelon"};
+
+        System.out.println(arr1);
+        System.out.println(numbers);
+        System.out.println(moreNumbers);
+        System.out.println(fruite);
+
+        for(int i=0; i<=arr1.length-1; i++){
+            System.out.println(arr1[i]);
+        }
+    }
+}
